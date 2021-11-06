e456244e947ac1d2008504d1f85e5d29463e9a22
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2021-11-06 17:30:07 -0400

    added

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import time
 import datetime
 import json
+import random
 
 
 
@@ -60,7 +61,41 @@ def get_times(route_name, url):
     return html
 
 
-url_daytime = 'https://mobi.mit.edu/default/transit/route?feed=nextbus&direction=loop&agency=mit&route=boston&_tab=stops'
+def get_data(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    m = re.findall('\n.*mins', soup.get_text())
+
+    now = datetime.datetime.now()
+
+    data = []
+    for g in m:
+        
+        s = g.split("Arriving in")
+
+        if len(s) < 2: continue
+        name = s[0].strip()
+        times = s[1].strip()
+
+        if len(times.split(",")) < 1 : continue
+        if len(times.split(",")[0].split(" ")) < 1 : continue
+        
+        mins_away = int(times.split(",")[0].split(" ")[0])
+
+        time_change = datetime.timedelta(minutes=mins_away)
+
+        arrival_time = now + time_change
+
+        arrival_time = arrival_time.strftime("%H:%M:%S")
+
+        data.append({
+            "name": name, 
+            "times": times, 
+            "mins_away": mins_away, 
+            "arrival_time": arrival_time})
+    return data
+
+url_daytime = 'https://mobi.mit.edu/default/transit/route?feed=nextbus&direction=loop&agency=mit&route=boston'
 
 url_beast = 'https://mobi.mit.edu/default/transit/route?feed=nextbus&direction=loop&agency=mit&route=saferidebostone'
 
@@ -73,6 +108,16 @@ def index():
     html += get_times("Saferide Boston East", url_beast)
     
     return html
+
+@app.route('/route_info')
+def route_info():
+        
+    response = {
+        "daytime": get_data(url_daytime),
+        "beast": get_data(url_beast)
+    }
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True, port=5000)
